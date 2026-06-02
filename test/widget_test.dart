ee64253216ced16da5a8395c9d32c85aa142f9be
Arc NineOhNine <arc@arcnineohnine.com>
2026-06-03 03:35:21 +1000

    updated widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,13 @@ import 'package:pia_wireguard_cfga/main.dart';
 void main() {
   testWidgets('shows PIA WireGuard config form', (tester) async {
     await tester.pumpWidget(const PiaWgApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('PIA WireGuard Config'), findsOneWidget);
     expect(find.text('Region ID'), findsOneWidget);
-    expect(find.text('PIA Username'), findsOneWidget);
-    expect(find.text('PIA Password'), findsOneWidget);
-    expect(find.text('DNS'), findsOneWidget);
+    expect(find.text('PIA username'), findsOneWidget);
+    expect(find.text('PIA password'), findsOneWidget);
+    expect(find.text('DNS servers'), findsOneWidget);
     expect(find.text('GENERATE CONFIG'), findsOneWidget);
     expect(find.byIcon(Icons.list_alt), findsOneWidget);
   });
