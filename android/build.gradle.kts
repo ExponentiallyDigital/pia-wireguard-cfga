ca1fdd81a3c0364cdf384dacb623f74bdc179464
Arc NineOhNine <arc@arcnineohnine.com>
2026-06-03 10:16:26 +1000

    v0.3.40+285 bump release version, updated release and build scripts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,14 @@
 // 1. Lock the plugins and build toolchain dependencies (What SonarQube wants)
 buildscript {
     dependencyLocking {
+        ignoredDependencies.add("io.flutter:*")
         lockAllConfigurations()
     }
 }
 
 // 2. Lock standard project configurations (Kept for completeness)
 dependencyLocking {
+    ignoredDependencies.add("io.flutter:*")
     lockAllConfigurations()
 }
 
@@ -27,12 +29,14 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
     dependencyLocking {
+        ignoredDependencies.add("io.flutter:*")
         lockAllConfigurations()
     }
 }
 subprojects {
     project.evaluationDependsOn(":app")
     dependencyLocking {
+        ignoredDependencies.add("io.flutter:*")
         lockAllConfigurations()
     }
 }
---
+++ build.ps1
@@ -0,0 +1,35 @@
+# Builder script
+Write-Host "Wiping old caches..." -ForegroundColor Cyan
+flutter clean
+
+Write-Host "Fetching dependencies..." -ForegroundColor Cyan
+flutter pub get --enforce-lockfile
+
+Write-Host "Adding icons..." -ForegroundColor Cyan
+dart run flutter_launcher_icons
+
+Write-Host "Compiling debug version..." -ForegroundColor Green
+flutter build apk --debug
+
+Write-Host "Compiling release version..." -ForegroundColor Green
+flutter build apk --release
+
+Write-Host "Compiling signed Android App Bundle (.aab) for Google Play..." -ForegroundColor Green
+flutter build appbundle --release
+
+Write-Host ""
+Write-Host "-------------------------------------------------------------------------------" -ForegroundColor DarkMagenta
+Write-Host "Play Store:   " -ForegroundColor White -NoNewline
+Write-Host ".\build\app\outputs\" -ForegroundColor Green -NoNewline
+Write-Host "bundle\release\" -ForegroundColor Cyan -NoNewline
+Write-Host "pia_wireguard_cfga-release.aab" -ForegroundColor Yellow
+Write-Host "Side loading: " -ForegroundColor White -NoNewline
+Write-Host ".\build\app\outputs\" -ForegroundColor Green -NoNewline
+Write-Host "flutter-apk\" -ForegroundColor Cyan -NoNewline
+Write-Host "app-release.apk" -ForegroundColor Yellow
+Write-Host "Debug:        " -ForegroundColor White -NoNewline
+Write-Host ".\build\app\outputs\" -ForegroundColor Green -NoNewline
+Write-Host "flutter-apk\" -ForegroundColor Cyan -NoNewline
+Write-Host "app-debug.apk" -ForegroundColor Yellow
+Write-Host "-------------------------------------------------------------------------------" -ForegroundColor DarkMagenta
+Write-Host ""
